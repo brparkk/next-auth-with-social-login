c64196aa8b0a914812de48bdb5a5ae682b2270f3
Rachel <borapark13@gmail.com>
2024-08-10 23:48:16 +0900

    feat: nextauth router settings.

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,8 @@
+import NextAuth, { AuthOptions } from "next-auth";
+
+const authOptions: AuthOptions = {
+  providers: [],
+};
+const handler = NextAuth(authOptions);
+
+export { handler as Get, handler as POST };
